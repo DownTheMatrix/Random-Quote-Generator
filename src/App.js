6249994f93a80c89939d2505b132bb8eb8620810
DownTheMatrix <suija87@gmail.com>
2018-11-12 19:52:46 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const quotes = [1, 2, 3, 4, 5];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      randomIndex: "",  // select a quote randomnly
+      quotes: "",  // get quote through API
+      author: ""   // get author through API
+    };
+    this.getQuote = this.getQuote.bind(this);
+  }
+
+  getQuote() {
+    setTimeout(() => {
+      this.setState({
+        randomIndex: Math.floor(Math.random() * quotes.length)
+      });
+    }, 500);
+  }
+
   render() {
+    const result = quotes[this.state.randomIndex];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+      <div className="page-wrapper">
+        <div id="quote-box">
+          <div id="text">&ldquo;{result}&rdquo;</div>
+
+          <p id="author">- Author</p>
+
+          <div className="social-sharing">
+            <a href="twitter.com/intent/tweet" id="tweet-quote">
+              <i className="fab fa-twitter-square" />
+            </a>
+            <a href="https://facebook.com" id="facebook-quote">
+              <i className="fab fa-facebook" />
+            </a>
+          </div>
+
+          <button id="new-quote" onClick={this.getQuote}>
+            New quote
+          </button>
+        </div>
+
+        <div className="credit">
           <a
-            className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/DownTheMatrix"
             target="_blank"
             rel="noopener noreferrer"
+            id="credit-link"
           >
-            Learn React
+            downthematrix
           </a>
-        </header>
+        </div>
       </div>
     );
   }
